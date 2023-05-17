61c8f2a6a914e1820e477b673ea194dec757a777
ducantalan960@gmail.com <ducantalan960@gmail.com>
2023-05-17 08:41:43 +0000

    final cte

--- models/core/fct_customer_orders_2.sql
+++ models/core/fct_customer_orders_2.sql
@@ -20,6 +20,18 @@ customers as (
 ),
 --Staging
 
+p as (
+
+    select 
+        orderid as order_id,
+        max(created) as payment_finalized_date,
+        sum(amount) / 100.0 as total_amount_paid
+    from payments
+    where status <> 'fail'
+    group by 1
+
+),
+
 paid_orders as (
         select orders.id as order_id,
             orders.user_id as customer_id,
@@ -30,15 +42,7 @@ paid_orders as (
             c.first_name as customer_first_name,
             c.last_name as customer_last_name
         from  orders
-            left join (
-        select 
-            orderid as order_id,
-            max(created) as payment_finalized_date,
-            sum(amount) / 100.0 as total_amount_paid
-        from payments
-        where status <> 'fail'
-        group by 1
-    ) p on orders.id = p.order_id
+            left join p on orders.id = p.order_id
     left join customers as c on orders.user_id = c.id ),
 
 customer_orders as (
@@ -50,7 +54,21 @@ customer_orders as (
     from customers as c 
     left join orders on orders.user_id = c.id 
     group by 1
-)
+),
+
+x as (
+
+    select
+        p.order_id,
+        sum(t2.total_amount_paid) as clv_bad
+    from paid_orders p
+    left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
+    group by 1
+    order by p.order_id
+
+),
+
+final as (
 
 select
     p.*,
@@ -63,14 +81,9 @@ select
     c.first_order_date as fdos
 from paid_orders p
 left join customer_orders as c using (customer_id)
-left outer join 
-(
-    select
-        p.order_id,
-        sum(t2.total_amount_paid) as clv_bad
-    from paid_orders p
-    left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
-    group by 1
-    order by p.order_id
-) x on x.order_id = p.order_id
+left outer join  x on x.order_id = p.order_id
 order by order_id
+
+)
+
+select * from final
